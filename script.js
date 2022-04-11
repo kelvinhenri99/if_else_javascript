bdc46416f6cb1d070cf16122a0456d8470ae07a4
kelvinhenri99 <kelvin5henri@gmail.com>
2022-04-11 10:55:46 -0300

    Implementação

---
+++ script.js
@@ -0,0 +1,19 @@
+function media(n1, n2) {
+    if (n1 + n2 >= 12){
+        console.log('Aprovado!');
+    } else {
+        console.log('Reprovado!');
+    }
+}
+
+media(6,6);
+
+const mediaFuncao = function (n3, n4) {
+    if (n3 + n4 >= 12) {
+        console.log('Aprovado!');
+    } else {
+        console.log('Reprovado!');
+    }
+}
+
+mediaFuncao(6,5);
